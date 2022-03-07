93ed05c1700a603e90a2e679c750695295e818ce
Anik Modak <anikmodak.rucse@gmail.com>
2022-03-08 00:32:05 +0600

    Added Model Controller Routes
    Added Model Controller Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StudentController::class, 'index'])->name('students.index');
+Route::get('/create', [StudentController::class, 'create'])->name('students.create');
+Route::post('', [StudentController::class, 'store'])->name('students.store');
+Route::get('/{id}/show', [StudentController::class, 'show'])->name('students.show');
+Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+Route::put('/{id}', [StudentController::class, 'update'])->name('students.update');
+Route::delete('/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
