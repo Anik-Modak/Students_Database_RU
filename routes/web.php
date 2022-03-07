93f6e890b0b67cfafe8a9acdf367976f65c6e223
Anik Modak <anikmodak.rucse@gmail.com>
2022-03-08 02:11:01 +0600

    Updated routes
    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [StudentController::class, 'index'])->name('students.index');
 Route::get('/create', [StudentController::class, 'create'])->name('students.create');
 Route::post('', [StudentController::class, 'store'])->name('students.store');
-Route::get('/{id}/show', [StudentController::class, 'show'])->name('students.show');
+Route::get('/{student}/show', [StudentController::class, 'show'])->name('students.show');
 Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
 Route::put('/{id}', [StudentController::class, 'update'])->name('students.update');
 Route::delete('/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
