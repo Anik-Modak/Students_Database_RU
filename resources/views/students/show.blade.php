d620ea65d90ca6b1ae0045432bb8725b6ee86eb8
Anik Modak <anikmodak.rucse@gmail.com>
2022-03-08 00:31:30 +0600

    Added view pages
    Added view pages

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layout',['title'=>'Show'])
+
+@section('page-content')
+
+    <table class="table table-striped">
+        <tr>
+            <th>Student</th>
+            <td>{{$student->s_id}}</td>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <td>{{$student->name}}</td>
+        </tr>
+        <tr>
+            <th>Department</th>
+            <td>{{$student->dept}}</td>
+        </tr>
+        <tr>
+            <th>Address</th>
+            <td>{{$student->address}}</td>
+        </tr>
+        <tr>
+            <th>Phone Number</th>
+            <td>{{$student->phone}}</td>
+        </tr>
+    </table>
+
+    <p class="text-start"><a href="{{route('students.index')}}" class="btn btn-primary">Back</a></p>
+@endsection
